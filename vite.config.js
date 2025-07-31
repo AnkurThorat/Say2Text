c292f7abcaed441c8d71cb06ad30cb86cf53f043
AnkurThorat <ankurthorat6@gmail.com>
2025-07-31 13:50:54 +0530

    Added Changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+  },
 });

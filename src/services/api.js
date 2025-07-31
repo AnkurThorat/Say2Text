f0b796caa9b26e7a8cfefd96c8d32cce871cd3ef
AnkurThorat <111000464+AnkurThorat@users.noreply.github.com>
2025-07-31 10:05:34 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://say2text-backend.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE,
